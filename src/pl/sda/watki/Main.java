7940cf12b2a8d39f689e7e0bfc1935b4dcd42bd1
Marek Blachowski <marekb2006@gmail.com>
2019-01-19 12:49:00 +0100

    Runner and Thread

--- src/pl/sda/watki/Main.java
+++ src/pl/sda/watki/Main.java
@@ -1,10 +1,60 @@
 package pl.sda.watki;
 
+class Runner implements Runnable {
+
+    long silnia(long n){
+        if (n<2) {
+            return 1;
+
+        }return n * silnia(n - 1);
+
+    }
+
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println("Hello: " + i + " thread: " + Thread.currentThread().getName());
+
+            silnia(i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        RunnerDouble runnerDouble = new RunnerDouble();
+        RunnerDouble runnerDouble1 = new RunnerDouble();
+        runnerDouble.start();
+        runnerDouble1.start();
+
+//        Thread thread = new Thread(new Runner());
+//        Thread thread1 = new Thread(new Runner());
+//        thread.start();
+//        thread1.start();
+//        RunnerExample runnerExample = new RunnerExample();
+//        RunnerExample runnerExample1 = new RunnerExample();
+//        runnerExample.start();
+//        runnerExample1.start();
+
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 100; i++) {
+//                    System.out.println("Hello: " + i + " thread: " + Thread.currentThread().getName());
+//
+//
+//                }
+//            }
+//        });
+//        thread.start();
+
 
     }
 }
